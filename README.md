1bfbb6264f1c63701c2415712260ab7ba51c5e4f
Troy <git@troycornwall.net>
2022-05-09 18:52:14 +1200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## BHGC Art generator
+
+Modified version of [hashlips_art_engine](https://github.com/HashLips/hashlips_art_engine)
---
+++ src/config.js
@@ -0,0 +1,120 @@
+require('dotenv').config();
+const basePath = process.cwd();
+const fs = require("fs");
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "BHGCTest2";
+const description = "Testing test test";
+const baseUri = "https://bhgc-dev.github.io/test/images"; // This will be replaced automatically
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 100,
+    layersOrder: [
+      { name: "backgrounds" },
+      { name: "bodies" },
+      { name: "tails" },
+      { name: "chests" },
+      { name: "legs" },
+      { name: "wings" },
+      { name: "eyes" },
+      { name: "wattles" },
+      { name: "beaks" },
+      { name: "glasses" },
+      { name: "clothes" },
+      { name: "sketchy" },
+      // { name: "accessories" },
+      { name: "hats" },    
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = true;
+
+const debugLogs = false;
+
+const format = {
+  width: 1000,
+  height: 1000,
+  smoothing: false,
+};
+
+const extraMetadata = {
+  external_url: "https://BHGC.no", // Replace with your website or remove this line if you do not have one.
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const rarityDelimiter = "$";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  gif,
+  preview_gif,
+};
