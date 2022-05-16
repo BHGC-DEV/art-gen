381da0a803c6511a810de412943b36b98854d4cc
Troy <git@troycornwall.net>
2022-05-16 18:12:13 +1200

    Update

--- utils/BirbAdlibs/index.js
+++ utils/BirbAdlibs/index.js
@@ -1,13 +1,43 @@
-import {Genders} from "./Genders";
-import {SingularPronouns, PossessivePronouns} from "./Pronouns";
-import {MaleNames} from "./MaleNames"
-import {FemaleNames} from "./FemaleNames"
+const basePath = process.cwd();
 
+const {BloodTypes} = require(`${basePath}/utils/BirbAdlibs/BloodTypes.js`);
+const {CommonAdjectives} = require(`${basePath}/utils/BirbAdlibs/CommonAdjectives.js`);
+const {EatingVerbs} = require(`${basePath}/utils/BirbAdlibs/EatingVerbs.js`);
+const {FemaleNames} = require(`${basePath}/utils/BirbAdlibs/FemaleNames.js`);
+const {Foods} = require(`${basePath}/utils/BirbAdlibs/Foods.js`);
+const {Genders} = require(`${basePath}/utils/BirbAdlibs/Genders.js`);
+const {GoodVerbs} = require(`${basePath}/utils/BirbAdlibs/GoodVerbs.js`);
+const {HatNames} = require(`${basePath}/utils/BirbAdlibs/HatNames.js`);
+const {HatProperties} = require(`${basePath}/utils/BirbAdlibs/HatProperties.js`);
+const {Jobs} = require(`${basePath}/utils/BirbAdlibs/Jobs.js`);
+const {MaleNames} = require(`${basePath}/utils/BirbAdlibs/MaleNames.js`);
+const {NightThings} = require(`${basePath}/utils/BirbAdlibs/NightThings.js`);
+const {NZPlaces} = require(`${basePath}/utils/BirbAdlibs/NZPlaces.js`);
+const {PejorativeVerbs} = require(`${basePath}/utils/BirbAdlibs/PejorativeVerbs.js`);
+const {PluralNouns} = require(`${basePath}/utils/BirbAdlibs/PluralNouns.js`);
+const {SingularPronouns} = require(`${basePath}/utils/BirbAdlibs/Pronouns.js`);
+const {PossessivePronouns} = require(`${basePath}/utils/BirbAdlibs/Pronouns.js`);
+const {Secrets} = require(`${basePath}/utils/BirbAdlibs/Secrets.js`);
+const {ZodiacSigns} = require(`${basePath}/utils/BirbAdlibs/ZodiacSigns.js`);
 
 module.exports = {
+    BloodTypes,
+    CommonAdjectives,
+    EatingVerbs,
+    FemaleNames,
+    Foods,
     Genders,
+    GoodVerbs,
+    HatNames,
+    HatProperties,
+    Jobs,
+    MaleNames,
+    NightThings,
+    NZPlaces,
+    PejorativeVerbs,
+    PluralNouns,
     SingularPronouns,
     PossessivePronouns,
-    MaleNames,
-    FemaleNames
+    Secrets,
+    ZodiacSigns
 }
--- utils/update_descriptions.js
+++ utils/update_descriptions.js
@@ -1,60 +1,177 @@
 const basePath = process.cwd();
 const fs = require("fs");
 const sentencer = require('sentencer');
-const mNames = require( '@stdlib/datasets-male-first-names-en' );
-const fNames = require( '@stdlib/datasets-female-first-names-en' );
+const {
+    BloodTypes,
+    CommonAdjectives,
+    EatingVerbs,
+    FemaleNames,
+    Foods,
+    Genders,
+    GoodVerbs,
+    HatNames,
+    HatProperties,
+    Jobs,
+    MaleNames,
+    NightThings,
+    NZPlaces,
+    PejorativeVerbs,
+    PluralNouns,
+    Secrets,
+    ZodiacSigns
+} = require(`${basePath}/utils/BirbAdlibs/index.js`);
 
 const {
     baseUri,
 } = require(`${basePath}/src/config.js`);
 
 let Gender =() => {
-    let random = Math.floor(Math.random() * 100)
-    if(random > 50)
-        return "M"
-    return "F"
+    return Math.floor( Math.random() * Genders.length );
 }
 
 let GenerateName = (gender) => {
-    if(gender === "M"){
-        let random = Math.floor(Math.random() * mNames().length)
-        return mNames()[random];
-    } else {
-        let random = Math.floor(Math.random() * fNames().length)
-        return fNames()[random];
+    if(gender === 1){
+        let random = Math.floor(Math.random() * MaleNames.length)
+        return MaleNames[random];
+    } else if (gender === 2 ) {
+        let random = Math.floor(Math.random() * FemaleNames.length)
+        return FemaleNames[random];
     }
+    let names = MaleNames.concat(FemaleNames);
+    let random = Math.floor(Math.random() * names.length)
+    return names[random];
 };
 
 let GenerateDescription = (gender, name) => {
-    let sentences = [
-        "{{name}} is the {{ adjective }} Bird of all. It has {{ an_adjective }} {{ nouns }}, and a {{ noun }} shaped like a {{ noun }}. It loves to eat {{ nouns }}, although it will feast on nearly anything. It is {{ adjective }} and {{ adjective }}. You must be {{ adjective }} around it, or you may end up as it`s meal!",
-        "This is an NFT of {{name}}, an original painting of a {{ adjective }} New Zealand {{ noun }}. {{He}} goes by {{name}} but {{his}} full name is {{name}} {{adjective}} {{noun}}. {{His}} blood type is {{noun}}. {{He}} is {{ an_adjective }}, but disdains {{ noun }} as \"not particularly useful for birds.\" {{He}} has a secret {{he}} can never tell, and a {{ noun }} {{he}} can never remove. During the day, {{name}} {{ adjective }} from branch to branch in the forest, {{ adjective }} up a {{ adjective }} with {{his}} {{ adjective }} {{ nouns }} and {{ adjective }} {{ noun }}. {{He}}'s the {{ adjective }} {{ noun }} in town. By night, {{he}} organizes {{ adjective }} {{ nouns }}. Sometimes, {{he}} {{ adjective }} at the {{ noun }}. {{name}}'s mission in life is to repay {{his}} Creator's mortgage in full. Once this {{ adjective }} service is performed, {{he}} will {{ adjective }} into the sun, {{ adjective }} at last."
-    ]
+    sentencer.configure({
+        actions: {
+            bloodtype : function() {
+                return BloodTypes[Math.floor( Math.random() * BloodTypes.length )];
+            },
+            commonAdjective : function() {
+                return CommonAdjectives[Math.floor( Math.random() * CommonAdjectives.length )];
+            },
+            eatingVerb : function() {
+                return EatingVerbs[Math.floor( Math.random() * EatingVerbs.length )];
+            },
+            food : function() {
+                return Foods[Math.floor( Math.random() * Foods.length )];
+            },
+            goodVerb : function() {
+                return GoodVerbs[Math.floor( Math.random() * GoodVerbs.length )];
+            },
+            hatName : function() {
+                return HatNames[Math.floor( Math.random() * HatNames.length )];
+            },
+            hatProperty : function() {
+                let list = HatProperties.concat(Secrets);
+                return list[Math.floor( Math.random() * list.length )];
+            },
+            job : function() {
+                return Jobs[Math.floor( Math.random() * Jobs.length )];
+            },
+            nightThing : function() {
+                return NightThings[Math.floor( Math.random() * NightThings.length )];
+            },
+            nzPlace : function() {
+                return NZPlaces[Math.floor( Math.random() * NZPlaces.length )];
+            },
+            pejorativeVerb : function() {
+                return PejorativeVerbs[Math.floor( Math.random() * PejorativeVerbs.length )];
+            },
+            pluralNoun : function() {
+                return PluralNouns[Math.floor( Math.random() * PluralNouns.length )];
+            },
+            secret : function() {
+                return Secrets[Math.floor( Math.random() * Secrets.length )];
+            },
+            zodiacSign : function() {
+                return ZodiacSigns[Math.floor( Math.random() * ZodiacSigns.length )];
+            },
+            initial: function (){
+                return String.fromCharCode(65 + Math.random() * 27);
+            }
 
-    let random = Math.floor(Math.random() * sentences.length)
-    var sentence = sentences[random];
+        }
+    });
 
-    sentence = sentence.replaceAll("{{name}}", name);
-    let he = "he";
-    let He = "He";
-    let His = "His";
-    let his = "his";
-    if(gender === "F"){
+    let sentence =
+        "This bird is part of the Bird Hat Grift Club, a collection of utterly unique, randomly generated birds (with hats.) Every bird has its own story." +
+        " This is {{name}}’s. \n\n" +
+        "This bird's full name is {{name}} {{ initial }} {{ hatName }}, but everyone calls it {{name}}." +
+        " {{He}} is the most {{ commonAdjective }} bird in {{ nzPlace }}. \n\n" +
+        "It loves to eat {{ food }} although it will {{ eatingVerb }} nearly anything.\n\n" +
+        "{{name}} is a {{ zodiacSign }} with a blood type of {{ bloodtype }} who {{ goodVerb }} {{ pluralNoun }} and {{ pluralNoun }}." +
+        " However, {{name}} {{ pejorativeVerb }} {{ pluralNoun }}. By day, {{name}} is a simple {{ job }}." +
+        " By night, {{he}} enjoys {{ nightThing }}. \n\n" +
+        "{{name}}’s secret is that {{he}} {{ secret }}. {{He}} has a hat that {{ hatProperty }}. The hat’s name is {{ hatName }}. \n";
+
+    let he = "it";
+    let He = "It";
+    let him = "it"
+    let Him = "It"
+    let his = "its"
+    let His = "Its"
+    if(gender === 1){ // M
+        he = "he";
+        He = "He";
+        him = "him"
+        Him = "Him"
+        his = "his"
+        His = "His"
+    } else if (gender === 2){ //F
         he = "she";
         He = "She";
-        His = "Her";
-        his = "her";
+        him = "her"
+        Him = "Her"
+        his = "hers"
+        His = "Hers"
     }
     sentence = sentence.replaceAll("{{name}}", name);
     sentence = sentence.replaceAll("{{he}}", he);
     sentence = sentence.replaceAll("{{He}}", He);
-    sentence = sentence.replaceAll("{{His}}", His);
+    sentence = sentence.replaceAll("{{him}}", him);
+    sentence = sentence.replaceAll("{{Him}}", Him);
     sentence = sentence.replaceAll("{{his}}", his);
+    sentence = sentence.replaceAll("{{His}}", His);
 
 
     return sentencer.make(sentence);
 };
 
+let getRating = () => {
+    return Math.floor( Math.random() * 5 ) +1;
+}
+
+let updateAttributes = (item, gender) => {
+    let Hat = getRating();
+    let Heart = getRating();
+    let Magic = getRating();
+    let Size = getRating();
+    let Speed = getRating();
+
+    updateAttribute(item, "Gender", Genders[gender]);
+    updateAttribute(item, "Hat", Hat);
+    updateAttribute(item, "Heart", Heart);
+    updateAttribute(item, "Magic", Magic);
+    updateAttribute(item, "Size", Size);
+    updateAttribute(item, "Speed", Speed);
+
+
+    for (let i = 0; i < item.attributes.length; i++) {
+        if(item.attributes[i].trait_type === "sketchy"
+            && item.attributes[i].value === "not sketchy")
+            item.attributes = item.attributes.filter(x=> x.trait_type !== "sketchy")
+    }
+}
+
+let updateAttribute = (item, attributeName, attributeValue) => {
+    item.attributes = item.attributes.filter(x=> x.trait_type !== attributeName)
+    item.attributes.push({
+        "trait_type": attributeName,
+        "value": attributeValue
+    })
+}
 
 (async () => {
     // read json data
@@ -67,6 +184,7 @@ let GenerateDescription = (gender, name) => {
         let g = Gender();
         let name = GenerateName(g);
         let description = GenerateDescription(g, name);
+        updateAttributes(item, g);
         item.name = name;
         item.description = description;
         item.image = `${baseUri}/${item.edition}.png`;
